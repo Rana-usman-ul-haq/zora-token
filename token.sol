faf05eb5412df740186f8dc5294b828c3ddd4c59
Rana-usman-ul-haq <98952692+Rana-usman-ul-haq@users.noreply.github.com>
2025-01-07 11:03:40 +0500

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
+/*
+
+Twitter: https://x.com/zoraerc?s=21
+Gitbook: https://zoraai.gitbook.io/zoraai
+Website: https://usezora.io/
+
+*/
+
 
 abstract contract Context {
     function _msgSender() internal view virtual returns (address) {
